1972ee2209314de4a57839ebd67ffcabb5114938
nuan ni <87644188+ninuan@users.noreply.github.com>
2023-03-30 05:49:39 +0000

    上传文件

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import requests
+from flask import Flask, render_template, jsonify
 
+app = Flask(__name__)
+
+
+@app.route('/')
+def first():
+  return render_template("home.html")
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
